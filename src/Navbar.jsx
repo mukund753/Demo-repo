8df75251eb5b42173e24821d1313d6d08b159256
mukund753 <mukundpatel753@gmail.com>
2023-03-08 11:56:46 +0530

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./navbar.css";
+
+function Navbar() {
+  return (
+      <div className="whole-navbar">
+          <div className="logo">
+              Mukund 
+          </div>
+
+          <div className="buttons-list">
+              <  ul className = "lists">
+                  <li>Home</li>
+                  <li>events</li>
+                  <li>Contact us</li>
+                  
+              </ul>
+          </div>
+
+          
+    </div>
+  )
+}
+
+export default Navbar
